788503a79879163a6a991b6cd2a9d10b24046135
zakalwesc <probably-genuine-interest@proton.me>
2026-02-25 17:12:57 -0800

    fix: remove unused useState import (build error)

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState } from 'react';
 import Link from 'next/link';
 import FeatureComparison from './FeatureComparison';
 import PricingSection from './PricingSection';
